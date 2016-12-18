5f732ebc191054da66f3103963a7e4597e142bdb
Mithun Vijayasekar <mithunvijayasekar@Mithuns-MacBook-Pro.local>
2016-12-18 14:16:00 -0800

    done with first project

--- first.js
+++ first.js
@@ -1,5 +1,34 @@
+//Contact list
+//Using two functions on contact objects
 
-// Check if the user is ready to play!
+var friends = {}
 
-confirm("are you ready to play?");
-console.log ("Hello World");
+friends.bill = {
+    firstName: "Bill",
+    lastName: "Gates",
+    number : 3215232342,
+    address : ["452 Microsoft Lane", "Seatle", "WA", "94204"]
+};
+
+friends.steve = {
+    firstName: "Steve",
+    lastName: "Jobs",
+    number : 4218459232,
+    address: ["133 Infinite Loop", "Cupertino", "CA", "02323"]
+};
+
+var list = function(friends){
+    for (var i in friends){
+        console.log(i);    
+    }    
+}
+
+var search = function(name){
+    for (var i in friends){
+        if (name === friends[i].firstName){
+            console.log(friends[i]);
+            return friends[i];
+        }    
+    }
+}
+
