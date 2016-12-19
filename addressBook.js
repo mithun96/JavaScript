82f89a937d98359c2a5399754efad309a4333bd8
Mithun Vijayasekar <mithunvijayasekar@Mithuns-MacBook-Pro.local>
2016-12-18 17:23:59 -0800

    second project

---
+++ addressBook.js
@@ -0,0 +1,57 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        printPerson(contacts[i]);
+    }
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+function search(lastName){
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++){
+        if (lastName = contacts[i].lastName){
+            printPerson(contacts[i])
+        }    
+    };
+};
+search();
+
+function add(firstName, lastName, email, phoneNumber){
+    var newPerson = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber
+    };
+    contacts[contacts.length]= newPerson;   
+    
+};
+add("Mithun","Vijay","xiaasdf3943@live.com.sg","9423349032")
+list()
+
+
+
+
+
+
